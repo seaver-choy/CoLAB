79c1fa760a16a87309b0acc069c6ce8154b9e91c
Carlo Gabriel Villalon Tapales <carlogabrielvillalon@gmail.com>
2016-09-03 07:41:37 +0800

    CoLAB Alpha 0.0.2
    - Splash Screen Complete -

--- app/src/main/java/com/anteriore/colab/SplashScreen.java
+++ app/src/main/java/com/anteriore/colab/SplashScreen.java
@@ -1,19 +1,26 @@
 package com.anteriore.colab;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    private TextView coLABSplash;
 
     @Override
     public void onCreate(Bundle icicle) {
 
         super.onCreate(icicle);
         setContentView(R.layout.activity_splash_screen);
+
+        coLABSplash = (TextView) findViewById(R.id.splash_welcome);
+        Typeface chalkduster = Typeface.createFromAsset(getAssets(), "fonts/chalkduster.ttf");
+        coLABSplash.setTypeface(chalkduster);
 
         new Handler().postDelayed(new Runnable() {
             @Override
