d4eeb3f646d9189409241eb2543a9c68f7347e29
Carlo Gabriel Villalon Tapales <carlogabrielvillalon@gmail.com>
2016-09-03 09:01:29 +0800

    CoLAB Alpha 0.0.3
    - Register and Login Items -

---
+++ app/src/main/java/com/anteriore/colab/RegisterActivity.java
@@ -0,0 +1,30 @@
+package com.anteriore.colab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText inputFullname;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private EditText confirmPassword;
+    private Button registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /*
+        setContentView(R.layout.activity_register);
+
+        inputFullname = (EditText) findViewById(R.id.register_input_fullname);
+        inputEmail = (EditText) findViewById(R.id.register_input_email);
+        inputPassword = (EditText) findViewById(R.id.register_input_password);
+        confirmPassword = (EditText) findViewById(R.id.register_confirm_password);
+        registerButton = (Button) findViewById(R.id.button_register);
+        */
+    }
+}
