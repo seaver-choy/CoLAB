2900629d4f43a9e1697c71dc607f9c90297d018d
seaver-choy <homenycablue@yahoo.com>
2016-09-03 09:51:17 +0800

    Added theoretical working of registering and signing in
    -Integration with firebase
    . Sign in
    . CreateUserWithFirebase

--- app/src/main/java/com/anteriore/colab/LoginActivity.java
+++ app/src/main/java/com/anteriore/colab/LoginActivity.java
@@ -1,23 +1,126 @@
 package com.anteriore.colab;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText inputUsername;
     private EditText inputPassword;
     private Button loginButton;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        auth = FirebaseAuth.getInstance();
+
         inputUsername = (EditText) findViewById(R.id.input_username);
-        inputUsername = (EditText) findViewById(R.id.input_password);
+        inputPassword = (EditText) findViewById(R.id.input_password);
         loginButton = (Button) findViewById(R.id.button_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                submitForm();
+            }
+        });
+    }
+
+    private void submitForm(){
+        String email = inputUsername.getText().toString().trim();
+        String password = inputPassword.getText().toString();
+
+        if(!checkEmail())
+        {
+            return;
+        }
+
+        if(!checkPassword())
+        {
+            return;
+        }
+
+        //loginInputLayoutEmail.setErrorEnabled(false);
+        //loginInputLayoutPassword.setErrorEnabled(false);
+
+        //progressBar.setVisibility(View.VISIBLE);
+        //authenticate user
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // If sign in fails, Log a message to the LogCat. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        //progressBar.setVisibility(View.GONE);
+                        if (!task.isSuccessful()) {
+                            // there was an error
+                            Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+
+                        } else {
+                            //Intent intent = new Intent(LoginActivity.this, UserActivity.class);
+                            //startActivity(intent);
+                            //finish();
+                        }
+                    }
+                });
+    }
+
+    private boolean checkEmail() {
+        String email = inputUsername.getText().toString().trim();
+        if (email.isEmpty() || !isEmailValid(email)) {
+
+            //inputUsername.setErrorEnabled(true);
+            //inputUsername.setError(getString(R.string.err_msg_email));
+            inputUsername.setError(getString(R.string.err_msg_required));
+            requestFocus(inputUsername);
+            return false;
+        }
+        //inputUsername.setErrorEnabled(false);
+        return true;
+    }
+
+    private boolean checkPassword() {
+
+        String password = inputPassword.getText().toString().trim();
+        if (password.isEmpty() || !isPasswordValid(password)) {
+
+            //loginInputLayoutPassword.setError(getString(R.string.err_msg_password));
+            inputPassword.setError(getString(R.string.err_msg_required));
+            requestFocus(inputPassword);
+            return false;
+        }
+        //loginInputLayoutPassword.setErrorEnabled(false);
+        return true;
+    }
+
+    private static boolean isEmailValid(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private static boolean isPasswordValid(String password){
+        return (password.length() >= 6);
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
     }
 }
--- app/src/main/java/com/anteriore/colab/RegisterActivity.java
+++ app/src/main/java/com/anteriore/colab/RegisterActivity.java
@@ -1,9 +1,21 @@
 package com.anteriore.colab;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -12,10 +24,13 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText inputPassword;
     private EditText confirmPassword;
     private Button registerButton;
-
+    private FirebaseAuth auth;
+    private String TAG = "RegisterActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
 
         /*
         setContentView(R.layout.activity_register);
@@ -26,5 +41,93 @@ public class RegisterActivity extends AppCompatActivity {
         confirmPassword = (EditText) findViewById(R.id.register_confirm_password);
         registerButton = (Button) findViewById(R.id.button_register);
         */
+
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitForm();
+            }
+        });
+    }
+
+    private void submitForm() {
+
+        String email = inputEmail.getText().toString().trim();
+        String password = inputPassword.getText().toString().trim();
+
+        if(!checkEmail()) {
+            return;
+        }
+        if(!checkPassword()) {
+            return;
+        }
+        //signupInputLayoutEmail.setErrorEnabled(false);
+        //signupInputLayoutPassword.setErrorEnabled(false);
+
+        //progressBar.setVisibility(View.VISIBLE);
+        //create user
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                        //progressBar.setVisibility(View.GONE);
+                        // If sign in fails, Log the message to the LogCat. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.d(TAG,"Authentication failed." + task.getException());
+
+                        } else {
+                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                            finish();
+                        }
+                    }
+                });
+        Toast.makeText(getApplicationContext(), "You are successfully Registered !!", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean checkEmail() {
+        String email = inputEmail.getText().toString().trim();
+        if (email.isEmpty() || !isEmailValid(email)) {
+
+            //signupInputLayoutEmail.setErrorEnabled(true);
+            //signupInputLayoutEmail.setError(getString(R.string.err_msg_email));
+            inputEmail.setError(getString(R.string.err_msg_required));
+            requestFocus(inputEmail);
+            return false;
+        }
+        //signupInputLayoutEmail.setErrorEnabled(false);
+        return true;
+    }
+
+    private boolean checkPassword() {
+
+        String password = inputPassword.getText().toString().trim();
+        if (password.isEmpty() || !isPasswordValid(password)) {
+
+            //signupInputLayoutPassword.setError(getString(R.string.err_msg_password));
+            inputPassword.setError(getString(R.string.err_msg_required));
+            requestFocus(inputPassword);
+            return false;
+        }
+        //signupInputLayoutPassword.setErrorEnabled(false);
+        return true;
+    }
+
+    private static boolean isEmailValid(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private static boolean isPasswordValid(String password){
+        return (password.length() >= 6);
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
     }
 }
