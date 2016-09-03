0b1b4da55a5400d089e6d1c7a742cfa162cd08b6
Carlo Gabriel Villalon Tapales <carlogabrielvillalon@gmail.com>
2016-09-03 08:44:26 +0800

    CoLAB Alpha 0.0.2
    - Splash Screen Activity with Items -

--- app/src/main/java/com/anteriore/colab/LoginActivity.java
+++ app/src/main/java/com/anteriore/colab/LoginActivity.java
@@ -2,12 +2,22 @@ package com.anteriore.colab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText inputUsername;
+    private EditText inputPassword;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inputUsername = (EditText) findViewById(R.id.input_username);
+        inputUsername = (EditText) findViewById(R.id.input_password);
+        loginButton = (Button) findViewById(R.id.button_login);
     }
 }
