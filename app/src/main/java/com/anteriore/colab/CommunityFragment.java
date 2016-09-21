300b322e9b95971359d883eec29ceaf3a1a11332
Carlo Gabriel Villalon Tapales <carlogabrielvillalon@gmail.com>
2016-09-21 18:21:40 +0800

    CoLAB Alpha 0.2.7
    - Added Community List -
    - Swipeable Notifications -
    - Fixed Like / Dislike Buttons -

--- app/src/main/java/com/anteriore/colab/CommunityFragment.java
+++ app/src/main/java/com/anteriore/colab/CommunityFragment.java
@@ -3,17 +3,40 @@ package com.anteriore.colab;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.util.ArrayList;
 
 public class CommunityFragment extends Fragment {
+
+    private RecyclerView connectionsRecyclerView;
+    private ProfileConnectionAdapter profileConnectionAdapter;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.community_view, container, false);
+
+        final ArrayList<ProfileConnection> connections = new ArrayList<>();
+
+        connections.add(new ProfileConnection("Chino Tapales", "10 connections", "26 common interests", R.drawable.profile_chino));
+        connections.add(new ProfileConnection("Chris Angping", "12 connections", "31 common interestes", R.drawable.profile_chris));
+        connections.add(new ProfileConnection("David Gamboa", "14 connections", "52 common interestes", R.drawable.profile_david));
+        connections.add(new ProfileConnection("Seaver Choy", "16 connections", "12 common interests", R.drawable.profile_seaver));
+
+        connectionsRecyclerView = (RecyclerView) v.findViewById(R.id.community_recyclerview);
+
+        profileConnectionAdapter = new ProfileConnectionAdapter(getContext(), connections);
+        connectionsRecyclerView.setLayoutManager(new LinearLayoutManager(v.getContext()));
+        connectionsRecyclerView.setAdapter(profileConnectionAdapter);
+
         return v;
     }
+
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
     }
+
 }
--- app/src/main/java/com/anteriore/colab/NotificationsFragment.java
+++ app/src/main/java/com/anteriore/colab/NotificationsFragment.java
@@ -1,10 +1,17 @@
 package com.anteriore.colab;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +21,7 @@ public class NotificationsFragment extends Fragment {
 
     private RecyclerView notificationsRecyclerView;
     private NotificationAdapter notificationsAdapter;
+    private Paint p = new Paint();
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.notifications_view, container, false);
@@ -32,12 +40,60 @@ public class NotificationsFragment extends Fragment {
         notificationsAdapter = new NotificationAdapter(getContext(), notifications);
         notificationsRecyclerView.setLayoutManager(new LinearLayoutManager(v.getContext()));
         notificationsRecyclerView.setAdapter(notificationsAdapter);
+        initSwipe();
 
         return v;
     }
 
+    private void initSwipe(){
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT){
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target){
+                return false;
+            }
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction){
+                int position = viewHolder.getAdapterPosition();
+                if(direction == ItemTouchHelper.LEFT){
+                }
+                else{
+                }
+            }
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive){
+                Bitmap icon;
+                if(actionState == ItemTouchHelper.ACTION_STATE_SWIPE){
+                    View itemView = viewHolder.itemView;
+                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                    float width = height / 3;
+                    if(dX > 0){
+                        p.setColor(Color.parseColor("#F5F5F5"));
+                        RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX,(float) itemView.getBottom());
+                        c.drawRect(background,p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.like);
+                        RectF icon_dest = new RectF((float) itemView.getLeft() + width ,(float) itemView.getTop() + width,(float) itemView.getLeft()+ 2*width,(float)itemView.getBottom() - width);
+                        c.drawBitmap(icon,null,icon_dest,p);
+                    }
+                    else{
+                        p.setColor(Color.parseColor("#F5F5F5"));
+                        RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(),(float) itemView.getRight(), (float) itemView.getBottom());
+                        c.drawRect(background,p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.dislike);
+                        RectF icon_dest = new RectF((float) itemView.getRight() - 2*width ,(float) itemView.getTop() + width,(float) itemView.getRight() - width,(float)itemView.getBottom() - width);
+                        c.drawBitmap(icon,null,icon_dest,p);
+                    }
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(notificationsRecyclerView);
+    }
+
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
+        notificationsRecyclerView.setAdapter(notificationsAdapter);
+        initSwipe();
     }
 }
