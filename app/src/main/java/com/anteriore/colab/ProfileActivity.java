5fb6be0e355fe5060182889b8de8d12c8a2fcfa2
Carlo Gabriel Villalon Tapales <carlogabrielvillalon@gmail.com>
2016-09-22 20:30:16 +0800

    CoLAB Alpha 0.3.1
    - Bug with Deck View in Profile Activity -

--- app/src/main/java/com/anteriore/colab/ProfileActivity.java
+++ app/src/main/java/com/anteriore/colab/ProfileActivity.java
@@ -4,12 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.anteriore.colab.Model.Interest;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -19,8 +24,12 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView quote;
     private Button backButton;
     private Button connectButton;
+    private RelativeLayout connectionsTab;
+    private RelativeLayout interestsTab;
     private RecyclerView connectionsRecyclerView;
+    private RecyclerView interestsRecyclerView;
     private ProfileConnectionAdapter profileConnectionAdapter;
+    private InterestProfileAdapter interestAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +43,31 @@ public class ProfileActivity extends AppCompatActivity {
         connections.add(new ProfileConnection("David Gamboa", "14 connections", "52 common interestes", R.drawable.profile_david));
         connections.add(new ProfileConnection("Seaver Choy", "16 connections", "12 common interests", R.drawable.profile_seaver));
 
+        final List<Interest> interests = new ArrayList<>();
+
+        interests.add(new Interest("LIKES", "interest_likes"));
+        interests.add(new Interest("PASSIONS", "interest_passion"));
+        interests.add(new Interest("HOBBIES", "interest_hobbies"));
+
+        interests.add(new Interest("LIKES", "interest_likes"));
+        interests.add(new Interest("PASSIONS", "interest_passion"));
+        interests.add(new Interest("HOBBIES", "interest_hobbies"));
+
+        interests.add(new Interest("LIKES", "interest_likes"));
+        interests.add(new Interest("PASSIONS", "interest_passion"));
+        interests.add(new Interest("HOBBIES", "interest_hobbies"));
+
+
         connectionsRecyclerView = (RecyclerView) findViewById(R.id.profile_activity_recyclerview);
+        interestsRecyclerView = (RecyclerView) findViewById(R.id.profile_activity_card_recyclerview);
         profileImage = (ImageView) findViewById(R.id.profile_activity_photo);
         profileName = (TextView) findViewById(R.id.profile_activity_name);
         profilePosition = (TextView) findViewById(R.id.profile_activity_field);
         quote = (TextView) findViewById(R.id.profile_activity_quote);
         backButton = (Button) findViewById(R.id.profile_activity_back_button);
         connectButton = (Button) findViewById(R.id.profile_activity_connect_button);
+        connectionsTab = (RelativeLayout) findViewById(R.id.profile_connections_button);
+        interestsTab = (RelativeLayout) findViewById(R.id.profile_common_interest_button);
 
         backButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -49,8 +76,28 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        connectionsTab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                interestsRecyclerView.setVisibility(View.GONE);
+                connectionsRecyclerView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        interestsTab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                interestsRecyclerView.setVisibility(View.VISIBLE);
+                connectionsRecyclerView.setVisibility(View.GONE);
+            }
+        });
+
         profileConnectionAdapter = new ProfileConnectionAdapter(this, connections);
         connectionsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         connectionsRecyclerView.setAdapter(profileConnectionAdapter);
+
+        interestAdapter = new InterestProfileAdapter(interests);
+        interestsRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
+        interestsRecyclerView.setAdapter(interestAdapter);
     }
 }
